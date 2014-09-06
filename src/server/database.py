c1359d928e96800dc7fedf5e4fecd17a0f82acb6
jbethune <jbethune@github.fake>
2014-09-06 20:45:43 +0200

    Created contents of database module

--- src/server/database.py
+++ src/server/database.py
@@ -1,5 +1,77 @@
 #!/usr/bin/env python
 from sys import argv, exit
+from util import slurp
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import backref, relationship
+
+from sqlalchemy import Column, String, Integer, ForeignKey, create_engine
+
+engine = create_engine( slurp( 'sql_engine.conf' ), echo = True )
+Base = declarative_base()
+
+def create_database():
+    Base.metadata.create_all(engine)
+
+def id_col():
+    return Column( Integer, primary_key = True )
+
+class User( Base ):
+    __tablename__ = 'user'
+    id = id_col()
+    name = Column( String )
+    salt = Column( String )
+    hashed_auth_data = Column( String )
+
+class Message( Base ):
+    __tablename__ = 'message'
+    id = id_col()
+    text = Column( String )
+    user_id = Column( Integer, ForeignKey( User.id ) )
+    context_id = Column( Integer, ForeignKey( 'context.id' ) )
+
+    user = relationship( User, backref = backref( 'messages' ) )
+    context = relationship( 'context' )
+
+class Service( Base ):
+    __tablename__ = 'service'
+    id = id_col()
+    name = Column( String )
+
+class Contact( Base ):
+    __tablename__ = 'contact'
+    id = id_col()
+    service_id = Column( Integer, ForeignKey( Service.id ) )
+    handle = Column( String )
+    user_id = Column( Integer, ForeignKey( User.id ) )
+
+    user = relationship( User, backref = 'contacts' )
+    service = relationship( Service )
+
+class Context( Base ):
+    __tablename__ = 'context'
+    id = id_col()
+    name = Column( String )
+
+class UserInContext( Base ):
+    __tablename__ = 'user_in_context'
+    id = id_col()
+    context_id = Column( Integer, ForeignKey( Context.id ) )
+    user_id = Column( Integer, ForeignKey( User.id ) )
+
+    #oh, the joy of many-to-many relationships
+    context = relationship( Context, backref = 'users_in_context' )
+    users = relationship( User, backref( 'user_in_contexts' ) )
+
+class ChatItem( Base ):
+    __tablename__ = 'chat_item'
+    id = id_col()
+    context_id = Column( Integer, ForeignKey( Context.id ) )
+    user_id = Column( Integer, ForeignKey( User.id ) )
+    message = Column( String )
+
+    context = relationship( Context, backref = 'items' )
+    author = relationship( User, backref = 'items' )
+
 
 if __name__ == "__main__":
-    pass
+    create_database()
