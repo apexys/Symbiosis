83b57a21d792ceae0137397a863cdfe645b87b1e
jbethune <jbethune@github.fake>
2014-09-06 13:30:48 +0200

    Added several data classes to chat.py
    Added some methods to the chat.User class

--- src/server/chat.py
+++ src/server/chat.py
@@ -2,6 +2,7 @@
 from sys import argv, exit
 from time import time
 from collections import namedtuple
+from enum import Enum, unique
 
 class ChatSession:
     """
@@ -15,12 +16,44 @@ class ChatSession:
 
 
 class User:
+    """A user that is registered in the web-messager.
+
+    A user is a meta-object that is associated with several accounts
     """
-    A user
-    """
-    pass
+    def __init__( self, username, password ):
+        #authenticate
+        self.accounts = {} #self-chosen name -> Account object
+        raise NotImplemented
+
+    def load_accounts( self ):
+        """
+        Populate self.accounts from the database
+        """
+        raise NotImplemented
+
+    def add_account( self, Account ):
+        """
+        Add account to the database and load it
+        """
+        raise NotImplemented
+
+    def connect( self, *account_names ):
+        """
+        Connect to services.
+
+        :param account_names: Connect the named accounts to their services. If no account_names are given, connect all accounts to their services
+        """
+        raise NotImplemented
+
+
+
+
 
 Message = namedtuple( 'Message', 'contact direction message' )
+Contact = namedtuple( 'Contact', 'plugin handle nick' )
+Account = namedtuple( 'Account', 'db_id state' ) #db_id references an ID in the database
+AccountState = Enum( 'AccountState', 'not_registered offline connecting connected' )
+
 
 if __name__ == "__main__":
     pass
