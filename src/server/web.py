301cc76c1279a82d3b180a65db6d3aad2e79c9e2
jbethune <jbethune@github.fake>
2014-09-07 10:54:29 +0200

    Login now queries the database
    But we first need to get users into the database

--- src/server/web.py
+++ src/server/web.py
@@ -2,23 +2,54 @@
 
 from sys import argv, exit
 from flask import Flask, session, escape, request, make_response
-from json import dumps as to_json
+from json import dumps
 from util import slurp
+from argparse import ArgumentParser
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+
+import database as db
 
 app = Flask( __name__ )
+sql = db.Session()
+
+def default_response():
+    return { 'infos': [], 'warnings': [], 'errors': [] }
+
+def to_json( f ):
+    def result( *args, **kwargs ):
+        return make_response( dumps( f( *args, **kwargs ) ) )
+    return result
 
 @app.route( '/' )
 def index():
     return slurp('../clients/static/index.html')
 
 @app.route( '/login', methods = ['POST'] )
+@to_json
 def login():
+    response = default_response()
     data = request.form
-    if data[ 'username' ] == 'dummy' and data[ 'password' ] == 'dummy':
-        response = make_response(to_json( { 'infos': [ 'Login successful' ] } ))
+    try:
+        db_user = sql.query( db.User ).filter( db.User.name == data[ 'username' ] ).one()
+    except NoResultFound:
+        response[ 'errors' ].append( 'Username not found' )
         return response
-    else:
-        return to_json( { 'errors': [ 'Login failed' ] } )
+
+    #TODO implement user password check
+    response[ 'infos' ].append( 'Login successful' )
+
+    return response
 
 if __name__ == "__main__":
-    app.run( debug = True )
+    parser = ArgumentParser()
+    parser.add_argument( '-d', '--debug', action = 'store_true',
+                         help = 'enable flask debug mode' )
+    parser.add_argument( '-c', '--create_database', action = 'store_true',
+                         help = 'create the database' )
+
+    args = parser.parse_args()
+    if args.create_database:
+        db.create_database()
+
+    app.run( debug = args.debug )
